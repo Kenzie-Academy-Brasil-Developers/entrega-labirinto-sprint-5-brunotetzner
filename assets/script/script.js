f6dc86cde203ab3cba956af52120a0e0d13f788e
Bruno Tetzner <brunoltetzner@gmail.com>
2021-10-02 12:07:33 -0300

    criaçao da mensagem de vitoria e começo de botao para jogar novamente

--- assets/script/script.js
+++ assets/script/script.js
@@ -57,6 +57,40 @@ for (let i = 0; i < map.length; i++) {
 }
 
 /*-----------------------------------------------------------*/
+function victory(){
+    allLabirinto.innerHTML = ' '
+    const containerVictory = document.createElement('div')
+    const containerVictoryBackground = document.createElement('div')
+    const containerVictoryBackgroundTwo = document.createElement('div')
+    const containerButton = document.createElement('div')
+    const victoryText = document.createElement('p')
+    const victoryTextTwo = document.createElement('p')
+    const playAgain = document.createElement('button')
+
+    containerVictory.classList.add('victoryContainer')
+    containerVictoryBackground.classList.add('victoryContainer__Background')
+    containerVictoryBackgroundTwo.classList.add('victoryContainer__Background')
+    victoryText.classList.add('victoryContainer__Text')
+    victoryTextTwo.classList.add('victoryContainer__Text')
+    containerButton.classList.add('container-button')
+    playAgain.classList.add('container-button__PlayAgain')
+
+    
+    containerVictory.appendChild(containerVictoryBackground)
+    containerVictory.appendChild(containerVictoryBackgroundTwo)
+    containerVictory.appendChild(playAgain)
+    containerVictoryBackground.appendChild(victoryText)
+    containerVictoryBackgroundTwo.appendChild(victoryTextTwo)
+
+
+    allLabirinto.appendChild(containerVictory)
+
+    victoryText.innerHTML = 'PARABÉNS guerreiro, você ganhou!' 
+    victoryTextTwo.innerHTML = 'Frodo te espera em Gondor para a coroação do novo rei.'
+    playAgain.innerHTML = 'Jogar Novamente'
+
+}
+
 //Jogador
 let playerPosition = [9,0]
 const player = document.createElement('div')
@@ -86,14 +120,23 @@ document.addEventListener('keydown', (event) => {
         player.style.left = boxHorizontal + "px";
     }
     else if (keyName === 'ArrowLeft' && map[playerPosition[0]][playerPosition[1]-1] === ' ') {
+        
         playerPosition[1] -= 1
         boxHorizontal -= 21
         player.style.left = boxHorizontal + "px";
+        
     }
-    else if (keyName === 'ArrowLeft' && map[playerPosition[0]][playerPosition[1]-1] === 'F') {
-        playerPosition[1] -= 1
-        boxHorizontal -= 21
+    //Condição de vitória
+    else if (keyName === 'ArrowRight' && mapAllItens[playerPosition[0]][playerPosition[1]+1] === 'F') {
+        playerPosition[1] += 1
+        boxHorizontal += 21
         player.style.left = boxHorizontal + "px";
+        victory()
+      //Criar um container no html
+      //selecionar via dom
+      //Criar um paragrafo e adicionar com filho do container
+      //adicionar texto ao pragrafo
+      //Mostrar na tela
     }
 });
 
